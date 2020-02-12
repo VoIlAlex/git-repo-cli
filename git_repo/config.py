b647ca8949dd33f9ad9019487f92feb6c065cde7
voilalex <ilya.vouk@gmail.com>
2020-02-12 08:48:28 +0300

    Add configuration file

---
+++ git_repo/config.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import pkg_resources
+from typing import List
+
+
+PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+# folder that holds all
+# the data of the package
+PACKAGE_DATA_PATH = None
+if sys.platform == 'linux':
+    PACKAGE_DATA_PATH = os.path.join('/var/git-repo')
+elif sys.platform == 'win32':
+    PACKAGE_DATA_PATH = os.path.join(os.getenv('APPDATA'), 'git-repo')
+
+LOG_FILE_PATH = os.path.join(PACKAGE_DATA_PATH, 'git-repo.log')
+
+GITIGNORE_STORE_PATH = os.path.join(PACKAGE_DATA_PATH, 'gitignore')
+
+ACCESS_TOKEN = '10973fe9b2d32d5b4d0ba95e3966925869badf95'
